570e4490bb74a709ea8fcb47f61f758703370a7b
Gogawa666 <gogawa@lcs.on.ca>
2021-01-29 12:29:46 -0500

    complete rectangle perimeter

--- PerimeterAreaVolumeHelper/main.swift
+++ PerimeterAreaVolumeHelper/main.swift
@@ -12,38 +12,58 @@ import Foundation
 /// - Parameter length: length of rectangle
 /// - Parameter width: width of rectangle
 /// - Returns: area of the rectangle
-func areaOfRectangle(length: Double, width: Double) -> Double {
+func areaOfRectangle(l: Double, w: Double) -> Double {
     
-    return length * width
+    return l * w
+    
+}
+/// Find the perimeter of rectangle
+/// - Parameter length: length of rectangle
+/// - Parameter width: width of rectangle
+/// - Returns: perimeter of the rectange
+func perimeterOfRectangle(l: Double, w: Double) -> Double {
+    
+    return (l + w) * 2
     
 }
 
 /// Find the area of circle
 /// - Parameter radius: the distance from the centre of circle to its outer edge
 /// - Returns: area of the circle
-func areaOfCircle(radius: Double) -> Double {
-    return Double.pi * pow(radius, 2.0)
+func areaOfCircle(r: Double) -> Double {
+    return Double.pi * r * r
 }
 
-/// Find the perimeter of rectangle
-/// - Parameter length: length of rectangle
-/// - Parameter width: width of rectangle
-/// - Returns: perimeter of the rectange
-func perimeterOfRectangle(length: Double, width: Double) -> Double {
+func perimeterOfCircle(radius: Double) -> Double {
     
-    return (length + width) * 2
+    return Double.pi * 2 * radius
     
 }
+
 
 //MARK: Input
 let givenLength = 10.0   // Set to a double using "type inference" (swift guesses the type)
 let givenWidth: Double = 2.0    // Set to a double using "type annotation" (swift guesses the type)
-
+let givenRadius = 3.0
+let givenHight = 4.0
+let givenSideleft = 5.0
+let givenSideright = 3.0
 
 //MARK: Process
-let area = areaOfRectangle(length: givenLength, width: givenWidth)
+let area = areaOfRectangle(l: givenLength, w: givenWidth)
+let peri = perimeterOfRectangle(l: givenLength, w: givenWidth)
+let areac = areaOfCircle(r: givenRadius)
+let peric = perimeterOfCircle(radius: givenRadius)
+
+
+
+
 
 //MARK: Output
-print("The area is \(area)")       //Example of "string interpolation"
+print("The area of lectangle is \(area)")       //Example of "string interpolation"
+print("The perimeter of lencangle is \(peri)")
+print("The area of circle is \(areac)")
+print("The perimeter of circle is \(peric)")
+
                                    
 
