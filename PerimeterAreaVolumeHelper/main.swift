24db71ec295bfc415201f40358266327de238264
Gogawa666 <gogawa@lcs.on.ca>
2021-01-30 16:15:04 -0500

    Completed functions for area and parameter of parallelogram

--- PerimeterAreaVolumeHelper/main.swift
+++ PerimeterAreaVolumeHelper/main.swift
@@ -17,6 +17,7 @@ func areaOfRectangle(l: Double, w: Double) -> Double {
     return l * w
     
 }
+
 /// Find the perimeter of rectangle
 /// - Parameter length: length of rectangle
 /// - Parameter width: width of rectangle
@@ -43,35 +44,59 @@ func perimeterOfCircle(r: Double) -> Double {
     
 }
 
-func areaOfTriangle(l: Double, w: Double) -> Double {
+/// Find the area of Triangle
+/// - Parameters:
+///   - l: height of the triangle
+///   - w: base of the triangle
+/// - Returns: area of the triangle
+func areaOfTriangle(h: Double, w: Double) -> Double {
     
-    return l * w * 1/2
+    return h * w * 1/2
     
 }
 
-func perimeterOfTriangle(W: Double, ls: Double, rs: Double) -> Double {
+/// Find the perimeter of triangle
+/// - Parameters:
+///   - W: base of the triangle
+///   - ls: left side of  the triangle
+///   - rs: right side of the triangle
+/// - Returns: perimeter of the triangle
+func perimeterOfTriangle(w: Double, ls: Double, rs: Double) -> Double {
     
-    return  ls + rs + W
+    return  ls + rs + w
     
 }
+
+func areaOfparallologram(w: Double, h: Double) -> Double {
+    
+    return w * h
+    
+}
+
+func perimeterOfparallologram(w: Double, ls: Double) -> Double {
+    
+    return w * 2 + ls * 2
+    
+}
+
 
 //MARK: Input
 
-
+let givenLength = 5.0
 let givenWidth = 2.0
 let givenRadius = 4.0
 let givenHight = 5.0
 let givenSideleft = 12.0
 let givenSideright = 3.0
-let givenLength = 10.0   // Set to a double using "type inference" (swift guesses the type)
+  
 
 //MARK: Process
 let area = areaOfRectangle(l: givenLength, w: givenWidth)
 let peri = perimeterOfRectangle(l: givenLength, w: givenWidth)
 let areac = areaOfCircle(r: givenRadius)
 let peric = perimeterOfCircle(r: givenRadius)
-let areat = areaOfTriangle(l: givenLength, w: givenWidth)
-let perit = perimeterOfTriangle(W: givenWidth, ls: givenSideleft, rs: givenSideright)
+let areat = areaOfTriangle(h: givenHight, w: givenWidth)
+let perit = perimeterOfTriangle(w: givenWidth, ls: givenSideleft, rs: givenSideright)
 
 
 //MARK: Output
@@ -90,6 +115,10 @@ print("The perimeter of lencangle is \(peri)")
 print("The area of circle is \(areac)")
 
 print("The perimeter of circle is \(peric)")
+
+print("The area of triangle is \(areat)")
+
+print("The perimeter of triangle is \(perit)")
 
 
 
