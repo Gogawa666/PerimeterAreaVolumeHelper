9b65d6198a6e719a176932dd459a35e66b9830fc
Gogawa666 <gogawa@lcs.on.ca>
2021-01-31 14:08:55 -0500

    Complete functions for perimeter and area of trapezoid

--- PerimeterAreaVolumeHelper/main.swift
+++ PerimeterAreaVolumeHelper/main.swift
@@ -67,36 +67,70 @@ func perimeterOfTriangle(w: Double, ls: Double, rs: Double) -> Double {
     
 }
 
-func areaOfparallologram(w: Double, h: Double) -> Double {
+/// Find the area of paralellogram
+/// - Parameters:
+///   - w: width of the paralellogram
+///   - h: hight of the paralellogram
+/// - Returns: area of the paralellogram
+func areaOfparallelogram(w: Double, h: Double) -> Double {
     
     return w * h
     
 }
 
-func perimeterOfparallologram(w: Double, ls: Double) -> Double {
+///  Find the perimeter of paralellogram
+/// - Parameters:
+///   - w: width of the paralellogram
+///   - ls:  side of the paralellogram
+/// - Returns: parameter of the paralellogram
+func perimeterOfparallelogram(w: Double, ls: Double) -> Double {
     
     return w * 2 + ls * 2
     
 }
 
+func areaOfTrapezoid(w: Double, h: Double, t: Double ) -> Double {
+    
+    return 0.5 * (w+t) * h
+    
+}
+
+/// Find the perimeter of trapezoid
+/// - Parameters:
+///   - w: bottom of the trapezoid
+///   - t: top of the trapezoid
+///   - ls: left side of the trapezoid
+///   - rs: right side of the trapezoid
+/// - Returns: the perimeter of trapezoid
+func perimeterOfTrapezoid(w: Double, t: Double, ls: Double, rs: Double) -> Double {
+    
+    return w + t + ls + rs
+    
+}
 
 //MARK: Input
 
 let givenLength = 5.0
-let givenWidth = 2.0
+let givenBottom = 2.0
 let givenRadius = 4.0
 let givenHight = 5.0
 let givenSideleft = 12.0
 let givenSideright = 3.0
+let givenTop = 5.0
+
   
 
 //MARK: Process
-let area = areaOfRectangle(l: givenLength, w: givenWidth)
-let peri = perimeterOfRectangle(l: givenLength, w: givenWidth)
+let area = areaOfRectangle(l: givenBottom, w: givenBottom)
+let peri = perimeterOfRectangle(l: givenLength, w: givenBottom)
 let areac = areaOfCircle(r: givenRadius)
 let peric = perimeterOfCircle(r: givenRadius)
-let areat = areaOfTriangle(h: givenHight, w: givenWidth)
-let perit = perimeterOfTriangle(w: givenWidth, ls: givenSideleft, rs: givenSideright)
+let areat = areaOfTriangle(h: givenHight, w: givenBottom)
+let perit = perimeterOfTriangle(w: givenBottom, ls: givenSideleft, rs: givenSideright)
+let areap = areaOfparallelogram(w: givenBottom, h: givenHight)
+let perip = perimeterOfparallelogram(w: givenBottom, ls: givenSideleft)
+let areatp = areaOfTrapezoid(w: givenBottom, h: givenHight, t: givenTop)
+let peritp = perimeterOfTrapezoid(w: givenBottom, ls: givenSideleft, rs: givenSideright, t: givenTop)
 
 
 //MARK: Output
